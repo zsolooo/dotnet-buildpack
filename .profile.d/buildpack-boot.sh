2d8d9c9350e1af9ac15940c4bc7e727b2ad37768
杜晓宇 <noliar@qq.com>
2015-09-05 15:32:32 +0800

    update buildpack-boot.sh

--- .profile.d/buildpack-boot.sh
+++ .profile.d/buildpack-boot.sh
@@ -1,4 +1,4 @@
-DNX_BIN=`find .dnx/runtimes -type d -name bin`
-export PATH="$PATH:/app/mono/bin:$DNX_BIN"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/mono/lib:/app/libuv/lib"
+DNX_BIN=`find $HOME/.dnx/runtimes -type d -name bin`
+export PATH="$PATH:$HOME/mono/bin:$DNX_BIN"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/mono/lib:$HOME/libuv/lib"
 export MONO_OPTIONS=--server
