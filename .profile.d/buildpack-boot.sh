57fb1c0a349d367b44b2fb55cdc4caac3035ae07
杜晓宇 <noliar@qq.com>
2016-03-13 12:14:39 +0800

    clear

--- .profile.d/buildpack-boot.sh
+++
@@ -1,8 +0,0 @@
-export PATH="$PATH:$HOME/mono/bin:$HOME/.apt/usr/bin:$DNX_BIN"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/mono/lib:$HOME/.apt/usr/lib/x86_64-linux-gnu:$HOME/.apt/usr/lib/i386-linux-gnu:$HOME/.apt/usr/lib"
-export LIBRARY_PATH="$LIBRARY_PATH:$HOME/.apt/usr/lib/x86_64-linux-gnu:$HOME/.apt/usr/lib/i386-linux-gnu:$HOME/.apt/usr/lib"
-export INCLUDE_PATH="$INCLUDE_PATH:$HOME/.apt/usr/include"
-export CPATH="$INCLUDE_PATH"
-export CPPPATH="$INCLUDE_PATH"
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$HOME/.apt/usr/lib/x86_64-linux-gnu/pkgconfig:$HOME/.apt/usr/lib/i386-linux-gnu/pkgconfig:$HOME/.apt/usr/lib/pkgconfig"
-export MONO_OPTIONS=--server
