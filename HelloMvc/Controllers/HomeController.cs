63e8405f37abfda346d13593b331fe4ebc578d75
杜晓宇 <noliar@qq.com>
2016-04-07 12:19:46 +0800

    To Show .NET CLI Version

--- HelloMvc/Controllers/HomeController.cs
+++ HelloMvc/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Hosting;
 
@@ -13,7 +15,36 @@ namespace HelloMvc
             ViewBag.ContentRoot = host.ContentRootPath;
             ViewBag.WebRoot = host.WebRootPath;
             ViewBag.Dir = Directory.GetCurrentDirectory();
+            string stdout;
+            RunProcessAndWaitForExit("dotnet", "--version", TimeSpan.FromSeconds(30), out stdout);
+            ViewBag.Version = stdout;
             return View();
         } 
+        
+        // source from dotnet/cli
+        private static int RunProcessAndWaitForExit(string fileName, string arguments, TimeSpan timeout, out string stdout)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            var process = Process.Start(startInfo);
+
+            stdout = null;
+            if (process.WaitForExit((int)timeout.TotalMilliseconds))
+            {
+                stdout = process.StandardOutput.ReadToEnd();
+            }
+            else
+            {
+                process.Kill();
+            }
+
+            return process.ExitCode;
+        }
     }
 }
