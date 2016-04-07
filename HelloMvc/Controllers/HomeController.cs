2b0e81d7c6c2aab5063b8d71a5a8427c83d2136f
杜晓宇 <noliar@qq.com>
2016-04-07 13:21:35 +0800

    Add Current, running CLI Version Badge

--- HelloMvc/Controllers/HomeController.cs
+++ HelloMvc/Controllers/HomeController.cs
@@ -15,11 +15,39 @@ namespace HelloMvc
             ViewBag.ContentRoot = host.ContentRootPath;
             ViewBag.WebRoot = host.WebRootPath;
             ViewBag.Dir = Directory.GetCurrentDirectory();
-            string stdout;
-            RunProcessAndWaitForExit("dotnet", "--version", TimeSpan.FromSeconds(30), out stdout);
-            ViewBag.Version = stdout;
+            ViewBag.Version = GetCliVersion();
             return View();
         } 
+        
+        [HttpGet("/badge")]
+        public ContentResult Badge()
+        {
+            var str = @"<svg xmlns='http://www.w3.org/2000/svg' width='270' height='20'>
+    <mask id='a'>
+        <rect width='270' height='20' rx='0' fill='#fff' />
+    </mask>
+    <g mask='url(#a)'>
+        <path fill='#555' d='M0 0h136v20H0z' />
+        <path fill='#007ec6' d='M136 0h134v20H136z' />
+        <path fill='url(#b)' d='M0 0h150v20H0z' />
+    </g>
+    <g fill='#fff' text-anchor='middle' font-family='DejaVu Sans,Verdana,Geneva,sans-serif' font-size='11'>
+        <text x='68' y='15' fill='#010101' fill-opacity='.3'>compatible cli version</text>
+        <text x='68' y='14'>compatible cli version</text>
+        <text x='205' y='15' fill='#010101' fill-opacity='.3'>{0}</text>
+        <text x='205' y='14'>{0}</text>
+    </g>
+</svg>";
+            var content = string.Format(str, GetCliVersion());
+            return new ContentResult{Content = content, ContentType="image/svg+xml; charset=utf-8; api-version=2.2"};
+        }
+        
+        private string GetCliVersion()
+        {
+            string stdout;
+            RunProcessAndWaitForExit("dotnet", "--version", TimeSpan.FromSeconds(30), out stdout);
+            return stdout;
+        }
         
         // source from dotnet/cli
         private static int RunProcessAndWaitForExit(string fileName, string arguments, TimeSpan timeout, out string stdout)
