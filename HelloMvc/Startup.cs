742d8333b68cccb9fe1b0b2833ca73da020878b8
杜晓宇 <noliar@qq.com>
2016-04-18 11:25:56 +0800

    Cleanup dependencies

--- HelloMvc/Startup.cs
+++ HelloMvc/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
